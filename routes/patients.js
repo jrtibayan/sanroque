c69f2d8ed85d7e33a7c53f5d66b4214f4d275eb0
Jeric <jrhod_baby@yahoo.com>
2021-01-23 21:42:20 +0800

    a cleaner patient registration

--- routes/patients.js
+++ routes/patients.js
@@ -1,42 +1,21 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
-const jwt = require('jsonwebtoken')
-const conf = require('config')
 
 const h = require('../misc/helper')
 
 const User = require('../models/user')
 
 function registerUser (newUser, res) {
-  User.getUserByEmail(
-    newUser.email,
-    (err, user) => {
-      h.dlog('Finding user with email: ' + newUser.email)
-
-      if (err) throw err
-
-      if (user) {
-        h.dlog('User already exist')
-        return res.json({ success: false, msg: 'User already exist' })
-      }
-
-      h.dlog('User not found. Will add the user')
-
-      h.dlog('Forward newUser to addUser function to add the user')
-      User.addPatient(newUser, (err, user) => {
-        if (err) {
-          h.dlog('Error adding user')
-          return res.json({ success: false, msg: 'Error adding user' })
-        } else {
-          h.dlog('User registered')
-          //h.emailRegistrationSuccessful(user.email, newPassword)
-
-          return res.json({ success: true, msg: 'User added' })
-        }
-      })
+  User.addPatient(newUser, (err, user) => {
+    if (err) {
+      h.dlog('Error adding user')
+      return res.json({ success: false, msg: 'Error adding user' })
     }
-  )
+
+    h.dlog('User registered')
+    return res.json({ success: true, msg: 'User added' })
+  })
 }
 
 // Register
